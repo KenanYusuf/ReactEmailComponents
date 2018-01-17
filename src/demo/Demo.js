7df56fe2792f4ae4ed3e46c609a68f90a9ca218c
Kenan Yusuf <kenan.yusuf@clock.co.uk>
2018-01-17 12:49:40 +0000

    > gets inline CSS working

--- src/demo/Demo.js
+++ src/demo/Demo.js
@@ -1,36 +1,29 @@
 import React, { Component } from 'react'
-
+import './Demo.styl'
 import { Container, Block, Row, Column, Spacer } from '../ReactEmailComponents'
 
 class App extends Component {
   render() {
     return (
-      <html>
-        <head>
-          <title>React Email Components</title>
-        </head>
-        <body>
-          <Container>
-            <Block className="test-block">
-              Test block
-            </Block>
+      <Container>
+        <Block className="test-block">
+          Test block
+        </Block>
 
-            <Spacer height={20} />
+        <Spacer height={20} />
 
-            <Row>
-              <Column padding={[ 5 ]} align="right">
-                Col 1
-              </Column>
-              <Column padding={[ 5 ]} align="right">
-                Col 2
-              </Column>
-              <Column padding={[ 5 ]} align="right">
-                Col 3
-              </Column>
-            </Row>
-          </Container>
-        </body>
-      </html>
+        <Row>
+          <Column padding={[ 5 ]} align="right">
+            Col 1
+          </Column>
+          <Column padding={[ 5 ]} align="right">
+            Col 2
+          </Column>
+          <Column padding={[ 5 ]} align="right">
+            Col 3
+          </Column>
+        </Row>
+      </Container>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ import ReactDOMServer from 'react-dom/server'
 import Demo from './demo/Demo'
 import registerServiceWorker from './registerServiceWorker'
 
-// const fs = require('fs')
-// const juice = require('juice')
+const juice = require('juice')
+const css = require('!css-loader!stylus-loader!./demo/Demo.styl') // eslint-disable-line import/no-webpack-loader-syntax
 
-// const juiceConfig = {
-//   'applyWidthAttributes': false,
-//   'applyHeightAttributes': false,
-//   'applyAttributesTableElements': false,
-//   'preserveImportant': true
-// }
+const juiceConfig = {
+  'applyWidthAttributes': false,
+  'applyHeightAttributes': false,
+  'applyAttributesTableElements': false,
+  'preserveImportant': true,
+  'extraCss': css
+}
 
-// const html = ReactDOMServer.renderToStaticMarkup(<Demo />)
-// const juiced = juice(html, juiceConfig)
+const html = ReactDOMServer.renderToStaticMarkup(<Demo />)
+const juiced = juice(html, juiceConfig)
 
-// console.log(juiced)
-
-ReactDOM.render(<Demo />, document.getElementById('root'))
+ReactDOM.render(<div dangerouslySetInnerHTML={{ __html: juiced }} />, document.querySelector('body'))
 registerServiceWorker()
